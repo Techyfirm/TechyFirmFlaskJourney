5fd4a74940579ddaf10539a535843d5af901cd75
Techyfirm <139818524+Techyfirm@users.noreply.github.com>
2023-10-21 02:45:23 +0100

    started learning dynamic data display making use of the templates blocks, which can be us for a lot of tags and attributes.

--- hello1.py
+++ hello1.py
@@ -2,10 +2,45 @@ from flask import Flask, Request, jsonify, render_template
 
 app = Flask(__name__)
 
+DESIGNS = [
+    {
+        'id': 1,
+        'title': 'WOTM',
+        'description': "In today's digital age, the demand for faster and more reliable communication technologies "
+                       "has reached unprecedented levels. From the early days of basic voice calls to the era of "
+                       "mobile broadband, each generation of wireless communication has brought significant "
+                       "advancements. However, as the world becomes increasingly connected and data-intensive "
+                       "applications continue to emerge, there is a pressing need for a transformative technology "
+                       "that can meet the demands of the future. This is where 5G technology comes into play.",
+
+    },
+    {
+        'id': 2,
+        'title': 'FAWC',
+        'description': "In today's digital age, the demand for faster and more reliable communication technologies "
+                       "has reached unprecedented levels. From the early days of basic voice calls to the era of "
+                       "mobile broadband, each generation of wireless communication has brought significant "
+                       "advancements. However, as the world becomes increasingly connected and data-intensive "
+                       "applications continue to emerge, there is a pressing need for a transformative technology "
+                       "that can meet the demands of the future. This is where 5G technology comes into play.",
+
+    },
+    {
+        'id': 3,
+        'title': 'TECHYFIRM',
+        'description': "In today's digital age, the demand for faster and more reliable communication technologies "
+                       "has reached unprecedented levels. From the early days of basic voice calls to the era of "
+                       "mobile broadband, each generation of wireless communication has brought significant "
+                       "advancements. However, as the world becomes increasingly connected and data-intensive "
+                       "applications continue to emerge, there is a pressing need for a transformative technology "
+                       "that can meet the demands of the future. This is where 5G technology comes into play.",
+
+    }
+]
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('home.html', designs=DESIGNS, company_name="TechyFirm")
 
 
 if __name__ == '__main__':
