96d53d95bff94711ab12b88af129b53b004149f8
Techyfirm <139818524+Techyfirm@users.noreply.github.com>
2024-05-05 22:59:41 +0100

    tryingto debug

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 from flask import Flask, jsonify, request
 
-from flask.ext.
+# from flask.ext
 
 app = Flask(__name__)
 
@@ -16,15 +16,15 @@ def get_info():
     current_day = datetime.date('current_day')
     utc_time = datetime.utcnow('utc_time')
     # Generate GitHub URLs based on input data
-    github_file_url = 
+    # github_file_url = 
     # Build JSON response
     response = {
         "slack_name": slack_name,
         "current_day": current_day,
         "utc_time": utc_time,
         "track": track,
-        "github_file_url": github_file_url,
-        "github_repo_url": github_repo_url,
+        # "github_file_url": github_file_url,
+        # "github_repo_url": github_repo_url,
         "status_code": 200
     }
 
